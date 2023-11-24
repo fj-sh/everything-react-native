1ffec5129d2f34de0c3fc644323d46a496c37cbf
fj <menspocky322@gmail.com>
2023-11-24 18:13:56 +0900

    blurred-scrollのサンプルを作成

---
+++ blurred-scroll/src/components/BlurredItem.tsx
@@ -0,0 +1,103 @@
+import Animated, {
+  Extrapolate,
+  Extrapolation,
+  interpolate,
+  useAnimatedStyle,
+  useDerivedValue,
+} from 'react-native-reanimated';
+import { BlurMask, Canvas, Group, LinearGradient, RoundedRect } from '@shopify/react-native-skia';
+
+interface BlurredItemProps {
+  index: number;
+  height: number;
+  width: number;
+  horizontalPadding?: number;
+  verticalPadding?: number;
+  contentOffsetY: Animated.SharedValue<number>;
+}
+
+const BlurredItem = ({
+  index,
+  horizontalPadding = 120,
+  verticalPadding = 120,
+  width,
+  height: blurredItemContainerHeight,
+  contentOffsetY,
+}: BlurredItemProps) => {
+  const inputRange = [
+    (index - 1) * blurredItemContainerHeight,
+    index * blurredItemContainerHeight,
+    (index + 1) * blurredItemContainerHeight,
+    (index + 2) * blurredItemContainerHeight,
+  ];
+
+  const blurOutputRange = [0.1, 0.1, 40, 0.1];
+
+  const blur = useDerivedValue(() => {
+    return interpolate(contentOffsetY.value, inputRange, blurOutputRange, Extrapolate.CLAMP);
+  }, []);
+
+  const rContainerStyle = useAnimatedStyle(() => {
+    const rotate = interpolate(
+      contentOffsetY.value,
+      inputRange,
+      [0, 0, Math.PI / 20, 0],
+      Extrapolate.CLAMP
+    );
+
+    return {
+      transform: [
+        {
+          rotate: `${rotate}rad`,
+        },
+      ],
+    };
+  }, []);
+
+  const transformGroup = useDerivedValue(() => {
+    const scale = interpolate(
+      contentOffsetY.value,
+      inputRange,
+      [0.8, 1, 0.8, 1],
+      Extrapolation.CLAMP
+    );
+
+    return [{ scale }];
+  }, []);
+
+  return (
+    <Animated.View style={rContainerStyle}>
+      <Canvas
+        style={{
+          width,
+          height: blurredItemContainerHeight,
+        }}
+      >
+        <Group
+          transform={transformGroup}
+          origin={{
+            x: width / 2,
+            y: 0,
+          }}
+        >
+          <RoundedRect
+            x={horizontalPadding / 2}
+            y={verticalPadding / 2}
+            width={width - horizontalPadding}
+            height={blurredItemContainerHeight - verticalPadding}
+            r={20}
+          >
+            <LinearGradient
+              start={{ x: 0, y: 0 }}
+              end={{ x: width, y: blurredItemContainerHeight }}
+              colors={['#9459F4', '#3411E4']}
+            />
+          </RoundedRect>
+          <BlurMask blur={blur} />
+        </Group>
+      </Canvas>
+    </Animated.View>
+  );
+};
+
+export { BlurredItem };
