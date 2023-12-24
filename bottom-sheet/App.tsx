44065dc571884967134f4b355f587d55c5fde043
fj <menspocky322@gmail.com>
2023-12-24 21:20:44 +0900

    bottomsheet をカスタマイズしてみた

--- bottom-sheet/App.tsx
+++ bottom-sheet/App.tsx
@@ -1,20 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import BottomSheet, { BottomSheetRefProps, MAX_TRANSLATE_Y } from './components/BottomSheet';
+import { useCallback, useRef } from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { LinearGradient } from 'expo-linear-gradient';
 export default function App() {
+  const ref = useRef<BottomSheetRefProps>(null);
+
+  const onPress = useCallback(() => {
+    const isActive = ref?.current?.isActive();
+    if (isActive) {
+      ref?.current?.scrollTo(0);
+    } else {
+      ref?.current?.scrollTo(MAX_TRANSLATE_Y);
+    }
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <LinearGradient colors={['#4c669f', '#3b5998', '#192f6a']} style={styles.container}>
+        <StatusBar style="light" />
+        <TouchableOpacity style={styles.button} onPress={onPress} />
+        <BottomSheet ref={ref}>
+          <View style={{ flex: 1, backgroundColor: 'orange' }} />
+        </BottomSheet>
+      </LinearGradient>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#111',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    height: 50,
+    borderRadius: 25,
+    aspectRatio: 1,
+    backgroundColor: 'white',
+    opacity: 0.6,
+  },
+  text: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  background: {
+    position: 'absolute', // 背景を画面全体にする
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
   },
 });
