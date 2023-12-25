be285140ca8bd9b0cf028dbe0ee2e99bae1154a0
fj <menspocky322@gmail.com>
2023-12-26 00:12:30 +0900

    movable-listを作り始めた

---
+++ movable-list/src/components/ListItem/index.tsx
@@ -0,0 +1,9 @@
+export type ItemInfo = {
+  title: string;
+  subtitle: string;
+  activeValues: boolean[];
+  color: string;
+  squareColor?: string;
+  textIcon: string;
+  isCompleted: boolean;
+};
